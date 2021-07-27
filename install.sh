25af80d9fb4c9b62f7b853cbdee815c515d23f38
Auke van Slooten <auke@muze.nl>
2021-07-27 13:16:10 +0200

    simple install/update script, needs bash

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git submodule update --init
 git submodule foreach 'git checkout editor'
 cd ..
 
-docker-compose exec graphql "sh -c 'cd json-graphql-server && npm install'"
-docker-compose exec graphql "sh -c 'npm run combine'"
+docker-compose exec graphql sh -c 'cd json-graphql-server && npm install'
+docker-compose exec graphql sh -c 'npm run combine'
 
-docker-compose exec rest-api "sh -c 'npm install'"
+docker-compose exec rest-api sh -c 'npm install'
