07a3e0a091891c85c70b7a380ddddef854da77d4
Auke van Slooten <auke@muze.nl>
2021-07-27 12:55:39 +0200

    start of installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ./curriculum-graphql-server
+git submodule update --init
+git submodule foreach 'git checkout editor'
+cd ..
+
+docker-compose exec graphql "sh -c 'cd json-graphql-server && npm install'"
+docker-compose exec graphql "sh -c 'npm run combine'"
+
+docker-compose exec rest-api "sh -c 'npm install'"
