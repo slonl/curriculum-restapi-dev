e32caebe1707d0177f87b9dfef2c8c9f60b209b9
govert@muze.nl <govert@muze.nl>
2023-07-20 13:31:33 +0200

    Moved REST api call to its own file, sadly the fetch to the website seems to be broken now

--- test/data/getData.mjs
+++ test/data/getData.mjs
@@ -3,59 +3,10 @@ import fs from "node:fs";
 
 let rootURL = "http://localhost:4500/";
 
-let APIcallsSLO = [
-  "doel", 
-  "niveau", 
-  "doelniveau",
-  "vakleergebied",
-  "kerndoel_vakleergebied", //Kerndoelen
-  "kerndoel_domein",
-  "kerndoel",
-  "kerndoel_uitstroomprofiel",
-  "examenprogramma", //ExamenProgramma
-  "examenprogramma_vakleergebied",
-  "examenprogramma_domein",
-  "examenprogramma_subdomein",
-  "examenprogramma_eindterm",
-  "examenprogramma_bg_profiel", // examenProgamma-bg
-  "examenprogramma_bg_kern",
-  "examenprogramma_bg_kerndeel",
-  "examenprogramma_bg_globale_eindterm",
-  "examenprogramma_bg_module",
-  "examenprogramma_bg_keuzevak",
-  "examenprogramma_bg_deeltaak",
-  "examenprogramma_bg_moduletaak",
-  "examenprogramma_bg_keuzevaktaak",
+// @TODO : some data was not retrievable, check if that still apllies, if not, add the following fields to REST_API_URLs.json
   // "syllabus",// curriculum-syllabus // does not give a compatible JSON file
-  "syllabus_vakleergebied",
-  "syllabus_specifieke_eindterm",
-  "syllabus_toelichting",
-  "syllabus_vakbegrip",
-  "ldk_vakleergebied", //curriculum-leerdoelenkaarten
-  "ldk_vakkern",
-  "ldk_vaksubkern",
-  "ldk_vakinhoud",
-  "ldk_vakbegrip",
-  "inh_vakleergebied", //curriculum-inhoudslijnen/
-  "inh_inhoudslijn",
-  "inh_cluster",
   // "inh_subcluster", // does not give a compatible json file.
-  "ref_vakleergebied", //curriculum-referentiekader/
-  "ref_domein",
-  "ref_subdomein",
-  "ref_onderwerp",
-  "ref_deelonderwerp",
-  "ref_tekstkenmerk",
-  "erk_vakleergebied", //curriculum-erk
-  "erk_gebied",
-  "erk_categorie",
-  "erk_taalactiviteit",
-  "erk_schaal",
-  "erk_candobeschrijving",
-  "erk_voorbeeld",
-  "erk_lesidee"
-  //niveau hierarchie
-];
+let APIcallsSLO = JSON.parse(fs.readFileSync(process.cwd() + "/REST_API_URLs.json"));
 
 async function getData(url = "", data = {}) {
   const response = await fetch(url, {
