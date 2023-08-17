d28699bf143babb02e4b33c3359a82fd302f667e
govert@muze.nl <govert@muze.nl>
2023-08-17 14:35:34 +0200

    fixed getData.mjs to handle errors when the returned JSON isn't correct.

--- test/data/getData.mjs
+++ test/data/getData.mjs
@@ -2,32 +2,30 @@ import tap from "tap";
 import fs from "node:fs";
 
 let rootURL = "http://localhost:4500/";
+let dummyData = { "dummyData" : "dummyData" }; // used when the returned JSON is incorrect/missing/empty
 
-// @TODO : some data was not retrievable, check if that still apllies, if not, add the following fields to REST_API_URLs.json
-  // "syllabus",// curriculum-syllabus // does not give a compatible JSON file
-  // "inh_subcluster", // does not give a compatible json file.
 let APIcallsSLO = JSON.parse(fs.readFileSync(process.cwd() + "/REST_API_URLs.json"));
 
 async function getData(url = "", data = {}) {
-  const response = await fetch(url, {
-    headers: {
-      "Accept": "application/json",
-      "Authorization": "Basic b3BlbmRhdGFAc2xvLm5sOjM1ODUwMGQzLWNmNzktNDQwYi04MTdkLTlmMGVmOWRhYTM5OQ=="
-    },
-  });
-  
-  return await response.json(); 
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Accept": "application/json",
+        "Authorization": "Basic b3BlbmRhdGFAc2xvLm5sOjM1ODUwMGQzLWNmNzktNDQwYi04MTdkLTlmMGVmOWRhYTM5OQ=="
+      },
+    });
+    return await response.json(); 
+  } catch (error){
+    console.log("did not get a correct JSON from: " + url + " -> error: " + error);
+    return dummyData;
+  }
 }
 
 async function gettheJSONData(call){
   let data = await getData(rootURL + call + "/");
-  if(JSON.stringify(data)){
-    console.log("busy with " + call);
-    fs.writeFileSync((process.cwd() + "/pages/" + call + ".json"), JSON.stringify(data));
-  }
-  else {
-    console.log("could not write " + call); 
-  }
+  JSON.stringify(data);
+  console.log("busy with " + call);
+  fs.writeFileSync((process.cwd() + "/pages/" + call + ".json"), JSON.stringify(data)); 
 }
 
 // go get the files using the rest API
