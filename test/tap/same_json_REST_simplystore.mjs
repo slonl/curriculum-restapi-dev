f606e38da316c3743067b32552460b0d491bbd57
govert@muze.nl <govert@muze.nl>
2023-09-06 16:03:30 +0200

    WIP cleaning data and parsing it in TAP

--- test/tap/same_json_REST_simplystore.mjs
+++ test/tap/same_json_REST_simplystore.mjs
@@ -2,22 +2,27 @@ import tap from "tap";
 import fs from "node:fs";
 
 //let rootURL = "http://localhost:4500/";
-//let rootURL = "https://opendata.slo.nl/curriculum/2022/api/v1/";
-let rootURL = "https://opendata.slo.nl/curriculum/api-acpt/v1/";
+let rootURL = "https://opendata.slo.nl/curriculum/2022/api/v1/";
+//let rootURL = "https://opendata.slo.nl/curriculum/api-acpt/v1/";
 
 let APIcallsSLO = JSON.parse(fs.readFileSync("../data/REST_API_URLs.json"));
 
 async function getData(url = "", data = {}) {
+  
   try {
     const response = await fetch(url, {
       headers: {
         "Accept": "application/json",
         "Authorization": "Basic b3BlbmRhdGFAc2xvLm5sOjM1ODUwMGQzLWNmNzktNDQwYi04MTdkLTlmMGVmOWRhYTM5OQ=="
       },
+      //timeout: 1000,
+      
     });
-        return await response.json();
+    
+    return await response.json();
+  
   } catch (error){
-    console.log("did not get correct response.json");
+    console.log("did not get correct response.json, or timed oud");
   }
 
 }
@@ -25,18 +30,29 @@ async function getData(url = "", data = {}) {
 //Check whether the API call and resulting files are identical
 for (let call of APIcallsSLO){
   let APICallData= JSON.parse(fs.readFileSync(process.cwd() + '/../data/pages/' +  call + ".json"));
-  let found = await getData(rootURL + call + "/");
   
-   console.log("This is a weird call: " + call + " because it is " + deepEqual(APICallData, found));
+  //console.log(APICallData);
+  
+  let found = await getData(rootURL + call + "/");
+  let wanted = APICallData;
 
-/*
+  //console.log(found);
+
+  found =  removeKeys(found);
+  wanted = removeKeys(APICallData); //APICallData; 
+
+  //console.log("The objects for: " + call + " are identical? " + deepEqual(APICallData, found));
+
   tap.test((call + ' comparison check'), async t => {
-      let found = await getData(rootURL + call + "/");
-      let wanted =  APICallData; 
+
+      //console.log(found);
+      //console.log(wanted);
+      console.log("The objects for: " + call + " are identical? " + deepEqual(wanted, found));
+      
       t.same(found, wanted)
       t.end();
   })
-*/
+
 
 }
 
@@ -46,4 +62,26 @@ function deepEqual(x, y) {
     ok(x).length === ok(y).length &&
       ok(x).every(key => deepEqual(x[key], y[key]))
   ) : (x === y);
+}
+
+function removeKeys(object) {
+  let discardKeyArray = ['@context', '@id', 'sloID', '$ref', 'description', 'bron', 'reference', 'prefix', 'deprecated', 'count', '@isPartOf', '@references', 'page', 'schema', 'replaces', '@type', 'replacedBy' ];
+  
+  Object.keys(object).forEach(function(key, index){
+    for(let keys in discardKeyArray){
+           
+      //console.log("This key: -" + key + "- Agains this array key: -" + discardKeyArray[keys] + "-");
+
+      if(key == discardKeyArray[keys]){
+        //console.log("deleting key " + key);  
+        delete object[key];
+      }
+   
+      if (typeof object[key] === 'object' && object[key] !== null){
+        //console.log("Moving down");
+        removeKeys(object[key]);
+      }
+    }
+  }) 
+  return object;
 }
