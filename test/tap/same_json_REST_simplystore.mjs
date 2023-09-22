92e36ad5e425297f7f124b0f805ffebf50c1c7fb
govert@muze.nl <govert@muze.nl>
2023-09-22 14:41:56 +0200

    cleaning and sanity checks

--- test/tap/same_json_REST_simplystore.mjs
+++ test/tap/same_json_REST_simplystore.mjs
@@ -5,15 +5,14 @@ let rootURL = "http://localhost:4500/";
 //let rootURL = "https://opendata.slo.nl/curriculum/2022/api/v1/";
 //let rootURL = "https://opendata.slo.nl/curriculum/api-acpt/v1/"; // do not use
 
-let discardKeyArray = ['@context', '@id', 'sloID', '$ref', 'description', 'bron', 'reference', 'prefix', 'deprecated', 'count', '@isPartOf', '@references', 'page', 'schema', 'replaces', '@type', 'replacedBy', 'error' ];
-let keepKeyArray = [];
+let discardKeyArray = ['Examenprogramma', 'ExamenprogrammaBgProfiel', 'erk_categorie_id', 'ExamenprogrammaDomein', 'LdkVakkern', 'type', 'RefOnderwerp','erk_candobeschrijving_id', 'erk_schaal_id', 'erk_taalactiviteit_id','isempty', 'unreleased', 'niveau_id', 'erk_voorbeeld_id', 'NiveauIndex', 'RefDomein', 'RefSubdomein', 'RefVakleergebied', 'erk_lesidee_id', '@context', '@id', 'sloID', 'error', '@type', 'deprecated' , '@ref', 'ce_se', 'Doel', 'description', 'bron', 'reference', 'prefix', 'count', '@isPartOf', '@references', 'page', 'schema', 'replaces', '@type', 'replacedBy', 'Niveau', 'SyllabusSpecifiekeEindterm', 'Syllabus', 'Vakleergebied'];
+//for backup purposes: let discardKeyArray = ['Examenprogramma', 'ExamenprogrammaBgProfiel', 'erk_categorie_id', 'ExamenprogrammaDomein', 'LdkVakkern', 'type', 'RefOnderwerp','erk_candobeschrijving_id', 'erk_schaal_id', 'erk_taalactiviteit_id','isempty', 'unreleased', 'niveau_id', 'erk_voorbeeld_id', 'NiveauIndex', 'RefDomein', 'RefSubdomein', 'RefVakleergebied', 'erk_lesidee_id', '@context', '@id', 'sloID', 'error', '@type', 'deprecated' , '@ref', 'ce_se', 'Doel', 'description', 'bron', 'reference', 'prefix', 'count', '@isPartOf', '@references', 'page', 'schema', 'replaces', '@type', 'replacedBy', 'Niveau', 'SyllabusSpecifiekeEindterm', 'Syllabus', 'Vakleergebied'];
+
+let keepKeyArray = ['uuid'];
 
 let APIcallsSLO = JSON.parse(fs.readFileSync("../data/REST_API_URLs.json"));
 
-//Check whether the API call and resulting files are identical
-//Todo: check if this needs to be a function.
-
-let yescounter = 0;
+let yescounter = 0; //counters for reporting if the files are identical
 let nocounter = 0;
 
 for (let call of APIcallsSLO){
@@ -22,22 +21,49 @@ for (let call of APIcallsSLO){
   let found = await getData(rootURL + call + "/");
   let wanted = APICallData;
 
-  found = removeKeys(found, discardKeyArray);
-  wanted = removeKeys(wanted, discardKeyArray);
+  
+  if(typeof found === 'object' && found != null){
+    found = removeKeysFromObject(found, discardKeyArray);
+  }
+
+  if(typeof wanted === 'object' && found != null){
+    wanted = removeKeysFromObject(wanted, discardKeyArray);
+  }
+
+
+
+  if(typeof found === 'object' && found != null){
+    found = keepKeysFromObject(found, keepKeyArray);
+  }
+
+  
+  if(typeof wanted === 'object' && wanted != null){
+    wanted = keepKeysFromObject(wanted, keepKeyArray);
+  }
+  
+
   
   if(deepEqual(wanted, found)){
-    console.log("yes for: " + call);
+    //console.log("YES FOR: " + call);
     yescounter++;
   }
   else{
-    console.log("no for: " + call);
     nocounter++;
+    console.log("FILES NOT EQUAL FOR: " + call)
+    console.log("FOUND:")
+    console.log(found);
+    console.log("WANTED:")
+    console.log(wanted);
+    console.log("");
   }
-
+  
+  /*
   tap.test((call + ' comparison check'), async t => {
       t.same(found, wanted)
       t.end();
   })
+  */
+
 }
 
 console.log("DeepEqual sanity check finds " + yescounter  + " identical files, and " + nocounter + " files being different.")
@@ -45,20 +71,33 @@ console.log("DeepEqual sanity check finds " + yescounter  + " identical files, a
 async function getData(url = "", data = {}) {
   
   try {
-    const response = await fetch(url, {
+    const response = await fetchWithTimeout(url, {
       headers: {
         "Accept": "application/json",
         "Authorization": "Basic b3BlbmRhdGFAc2xvLm5sOjM1ODUwMGQzLWNmNzktNDQwYi04MTdkLTlmMGVmOWRhYTM5OQ=="
-      },
-      //timeout: 1000,
-      
+      }
     });
     
     return await response.json();
   
   } catch (error){
-    console.log("did not get correct response.json, or timed out");
+    console.log("Did not get correct response.json, or timed out for: " + url);
   }
+}
+
+async function fetchWithTimeout(resource, options = {}) {
+  const { timeout = 1000 } = options;
+  
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal  
+  });
+  clearTimeout(id);
+
+  return response;
 }
 
 function deepEqual(x, y) {
@@ -69,31 +108,31 @@ function deepEqual(x, y) {
   ) : (x === y);
 }
 
-function removeKeys(obj, discardKeyArray){
-  var index;
-  for (var prop in obj) {
-      if(obj.hasOwnProperty(prop)){
-          switch(typeof(obj[prop])){
-              case 'string':
-                index = discardKeyArray.indexOf(prop);
-                  for (let key in discardKeyArray){
-                  if(index > -1 && prop == discardKeyArray[key]){ 
-                      delete obj[prop];
-                  }
-                }
-                break;
-              case 'object':
-                index = discardKeyArray.indexOf(prop);
-                for (let key in discardKeyArray){
-                  if(index > -1 && prop == discardKeyArray[key]){ 
-                      delete obj[prop];
-                  }else{
-                      removeKeys(obj[prop], discardKeyArray);
-                  }
-                } 
-                break;
-          }
-      }
-  }
-  return obj;
+function keepKeysFromObject(subject, keepKeyArray) {
+  Object.keys(subject).forEach(key => {
+    if (keepKeyArray.includes(key)) {
+      //console.log("keeping key: " + subject[key]);
+      keepKeysFromObject(subject[key], keepKeyArray);
+    } else if (typeof subject[key] === 'object' && subject[key] !== null) {
+      delete subject[key];
+      //console.log("deleting key: " + key);
+    }
+  })
+  return subject;
 }
+
+
+  function removeKeysFromObject(subject, discardKeyArray) {
+    Object.keys(subject).forEach(key => {
+      if (discardKeyArray.includes(key)) {
+        delete subject[key];
+        //console.log("deleting key: " + key);
+      } else if (typeof subject[key] === 'object' && subject[key] !== null) {
+        removeKeysFromObject(subject[key], discardKeyArray)
+        //console.log("Moving deeper: " + removingKeysCounter++);
+      }
+    })
+    return subject;
+  }
+
+
