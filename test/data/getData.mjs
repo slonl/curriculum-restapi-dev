0c48f44fc5006b0e997d7a503fa75950c9558d18
govert@muze.nl <govert@muze.nl>
2023-10-05 11:07:31 +0200

    getData now makes folders for local and 2022 data

--- test/data/getData.mjs
+++ test/data/getData.mjs
@@ -1,10 +1,38 @@
-import tap from "tap";
 import fs from "node:fs";
 
-let rootURL = "http://localhost:4500/"; //https://opendata.slo.nl/curriculum/api-acpt/v1/   // "https://opendata.slo.nl/curriculum/2022/api/v1/"; // 
+let localRootURL = "http://localhost:4500/"; //https://opendata.slo.nl/curriculum/api-acpt/v1/ 
+let remoteRootURL = "https://opendata.slo.nl/curriculum/2022/api/v1/";
+//let currentRootURL = https://opendata.slo.nl/curriculum/api-acpt/v1/ 
+
 let dummyData = { "dummyData" : "dummyData" }; // used when the returned JSON is incorrect/missing/empty
 
 let APIcallsSLO = JSON.parse(fs.readFileSync(process.cwd() + "/REST_API_URLs.json"));
+
+let rootPagesFolder = "/pages"
+let localDataFolder = "/pages/localhost";
+let remoteDatafolder = "/pages/remote";
+let localDataFolderUUIDS = "/pages/localhost/uuid";
+let remoteDatafolderUUIDS = "/pages/remote/uuid";
+
+try {
+  if (!fs.existsSync(process.cwd() + rootPagesFolder)) {
+    fs.mkdirSync(process.cwd() + rootPagesFolder);
+  }
+  if (!fs.existsSync(process.cwd() + localDataFolder)) {
+    fs.mkdirSync(process.cwd() + localDataFolder);
+  }
+  if (!fs.existsSync(process.cwd() + remoteDatafolder)) {
+    fs.mkdirSync(process.cwd() + remoteDatafolder);
+  }
+  if (!fs.existsSync(process.cwd() + localDataFolderUUIDS )) {
+    fs.mkdirSync(process.cwd() + localDataFolderUUIDS);
+  }
+  if (!fs.existsSync(process.cwd() + remoteDatafolderUUIDS)) {
+    fs.mkdirSync(process.cwd() + remoteDatafolderUUIDS);
+  }
+} catch (err) {
+  console.error(err);
+}
 
 async function getData(url = "", data = {}) {
   try {
@@ -21,14 +49,22 @@ async function getData(url = "", data = {}) {
   }
 }
 
-async function gettheJSONData(call){
-  let data = await getData(rootURL + call + "/");
+async function getLocalJSONData(call){
+  let data = await getData(localRootURL + call + "/");
   JSON.stringify(data);
   console.log("busy with " + call);
-  fs.writeFileSync((process.cwd() + "/pages/" + call + ".json"), JSON.stringify(data)); 
+  fs.writeFileSync((process.cwd() + localDataFolder + "/" + call + ".json"), JSON.stringify(data)); 
+}
+
+async function getRemoteJSONData(call){
+  let data = await getData(remoteRootURL + call + "/");
+  JSON.stringify(data);
+  console.log("busy with " + call);
+  fs.writeFileSync((process.cwd() + remoteDatafolder + "/" + call + ".json"), JSON.stringify(data)); 
 }
 
 // go get the files using the rest API
 for (let call of APIcallsSLO){
-  gettheJSONData(call);
+  getLocalJSONData(call);
+  getRemoteJSONData(call);
 }
