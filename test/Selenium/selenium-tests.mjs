99773d57e151edea942d2912591a9303b955e9e9
govert@muze.nl <govert@muze.nl>
2024-10-23 16:48:42 +0200

    headless chrome selenium webdriver test and connection test

---
+++ test/Selenium/selenium-tests.mjs
@@ -0,0 +1,42 @@
+import { Builder, By, Key, until, Browser } from 'selenium-webdriver';
+import tap from "tap";
+//import * as fs from 'fs';
+import Chrome from 'selenium-webdriver/chrome.js';
+
+
+(async function firstTest() {
+  let driver;
+  
+  try {
+
+       const screen = {
+      width: 640,
+      height: 480
+    };
+
+    driver = new Builder()
+        .forBrowser(Browser.CHROME)
+        .setChromeOptions(new Chrome.Options().addArguments('--headless').windowSize(screen))
+        .build();
+
+
+    await driver.get('http://localhost:4500');
+
+    tap.test("Website online", async t => {
+      
+      //Check website online
+      let title = await driver.getTitle();
+      t.equal("SLO Curriculum Browser", title);
+      //document weergave flow check
+
+
+      //spreadsheet weergave flow check
+      
+    })
+
+  } catch (e) {
+    console.log(e)
+  } finally {
+    await driver.quit();
+  }
+}())
