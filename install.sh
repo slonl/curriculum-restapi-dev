cb3381ca335cc59d15ead90d3dcdf97242ccab6a
govert@muze.nl <govert@muze.nl>
2025-01-28 14:08:32 +0100

    new installation script, removing graphql and updates DB on install

--- install.sh
+++ install.sh
@@ -1,10 +1,30 @@
-#!/bin/bash
-cd ./curriculum-graphql-server
-git submodule update --init
-git submodule foreach 'git checkout editor'
+#!/usr/bin/env bash
+
+set -o errexit  # Exit script when a command exits with non-zero status.
+set -o errtrace # Exit on error inside any functions or sub-shells.
+set -o nounset  # Exit script on use of an undefined variable.
+set -o pipefail # Return exit status of the last command in the pipe that exited with a non-zero exit code
+
+git clone https://github.com/slonl/curriculum-rest-api.git
+cd curriculum-rest-api
+git pull
+npm update
+cd  ..
+git clone https://github.com/slonl/curriculum-search-server.git
+cd curriculum-search-server
+git pull
+npm update
 cd ..
-
-docker-compose exec graphql sh -c 'cd json-graphql-server && npm install'
-docker-compose exec graphql sh -c 'npm run combine'
-
-docker-compose exec rest-api sh -c 'npm install'
+git clone https://github.com/slonl/curriculum-store.git
+cd curriculum-store
+git pull
+cd scripts
+./init.sh
+cd ..
+npm update
+cd scripts
+node tojsontag.mjs
+node convert.mjs ../data/curriculum.jsontag ../data/data.json
+cd ..
+npm update
+echo project installation completed succesfully, you can now start docker with "docker compose up"
