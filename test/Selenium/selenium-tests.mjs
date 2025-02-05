5a04674389e7118597087a9cdb17edbb6290001d
govert@muze.nl <govert@muze.nl>
2025-02-05 16:55:36 +0100

    Selenium test WIP due to buttons missing in spreadsheet

--- test/Selenium/selenium-tests.mjs
+++ test/Selenium/selenium-tests.mjs
@@ -45,6 +45,13 @@ const options = new Firefox.Options();
       let title = await driver.getTitle();
       t.equal("SLO Curriculum Browser", title)   
     })
+
+    tap.test("Api Calls: Checking known curricula are online", async t => {
+      for(let call of APIcallsSLO){
+        let data = await getData(localRootURL + call + "/");
+        t.ok(data, ("Response for: " +  call))
+      }
+    })
     
 
     tap.test("Test Entiteiten CRUD", async t => {
@@ -58,6 +65,8 @@ const options = new Firefox.Options();
           t.fail(('Could not navigate to: ' + call)) // @TODO: should get triggered on timeout in navigations 
         }
 
+        
+
         try {
           await driver.wait(
             until.elementLocated(By.css("h1[data-simply-field='listTitle']")),
@@ -66,19 +75,56 @@ const options = new Firefox.Options();
         } catch (error) {
           t.fail(('Timeout loading page for: ' + call))
         }
-      }
-    })
 
-    
-    
-    tap.test("Api Calls: Checking known curricula are online", async t => {
-      for(let call of APIcallsSLO){
-        let data = await getData(localRootURL + call + "/");
-        t.ok(data, ("Response for: " +  call))
-      }
-    })
-    
+        let browserURL = await driver.getCurrentUrl()
+        console.log("URL:", browserURL)
+        let linkElements = await driver.findElements(By.css('a.slo-relatie'));
 
+        await linkElements[0].click()      
+        
+        let spreadsheetButtonElement = await driver.findElement(By.css('[data-simply-command="switchView"][data-simply-value="spreadsheet"]'));
+      
+        await spreadsheetButtonElement.click()
+        
+
+        try {
+          await driver.wait(
+            until.elementLocated(By.css("tbody")),
+            2000 // Maximum wait time in milliseconds
+          );
+        } catch (error) {
+          t.fail(('Timeout loading page for tbody: ' + call))
+        }
+        
+        const bodyText = await driver.findElement(By.css('tbody')).getText();
+        
+
+        //debugging
+        if(bodyText.includes("Wiskunde")){
+          console.log("SPREADSHEET LOADED")
+        }
+
+        
+
+        // let svgElement = await driver.wait(
+        //     until.elementLocated(By.css(`svg[data-simply-command="insertRow"]`)),
+        //     5000 // Maximum wait time in milliseconds
+        //   );
+
+          let svgElement = await driver.findElements(By.css(`svg[data-simply-command="insertRow"]`))[0]
+      
+          let actions = driver.actions({ bridge: true});
+          await actions.move({ origin: svgElement}).click().perform()
+          
+         //await driver.executeScript("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", svgElement[0]);
+          
+          //await svgElement[0].click()
+        //let addRowButton = await driver.findElement(By.css(`svg[data-simply-command="insertRow"]`))
+
+        // console.log("add row button?:", await addRowButton )
+      }
+    }) 
+    
   } catch (e) {
     console.log(e)
   } finally {
