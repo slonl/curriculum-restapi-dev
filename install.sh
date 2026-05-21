9d4d531875019c8b567e4e452990e43d6c7569de
Auke van Slooten <auke@muze.nl>
2026-05-21 13:10:53 +0200

    improved error checking and bash startup

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit -o errtrace -o nounset -o pipefail
+
 git submodule update --init
 cd curriculum-rest-api
 cp apiKeys.start.json apiKeys.json
 cd ..
+echo "done";
