cb64eeb47d51157bb31de75dd4f755eae56cf6a3
Auke van Slooten <auke@muze.nl>
2026-05-21 13:08:27 +0200

    removed npm stuff, that is already in the docker-compose file

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 git submodule update --init
-docker compose exec rest-api sh -c 'cd /home/node/app && npm install'
-docker compose exec search-server sh -c 'cd /home/node/app && npm install'
-docker compose exec curriculum-store sh -c 'cd /home/node/app && npm install'
-docker compose exec curriculum-registration sh -c 'cd /home/node/app && npm install'
-docker compose exec rest-api sh -c 'cd /home/node/app && cp apiKeys.start.json apiKeys.json'
+cd curriculum-rest-api
+cp apiKeys.start.json apiKeys.json
+cd ..
